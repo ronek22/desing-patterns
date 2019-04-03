52978acc3d3f8aeb89a5387613406bc46cb837c2
ronek22 <jronek3010@gmail.com>
2019-04-03 18:51:40 +0200

    Factories done

--- src/main/java/Products/Furniture.java
+++
@@ -1,9 +0,0 @@
-package Products;
-
-public abstract class Furniture {
-    protected String name;
-    protected String material;
-
-    public abstract void build();
-    public abstract void deliver();
-}
